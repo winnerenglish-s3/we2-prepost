9bb57526629b07ac9fdc4df56c002f4f90729af0
MMasterZ <soulhunz@hotmail.com>
2020-10-27 13:59:37 +0700

    create Project Prepost

---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <span class="text-h1">Pre Post</span>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
