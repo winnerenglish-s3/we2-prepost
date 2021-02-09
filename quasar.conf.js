d1e76eb18455c7530b73bfbeb8d2bc9416069f2d
Vitas <ber_zebub@hotmail.com>
2021-02-09 16:37:21 +0700

    UPDATE

--- quasar.conf.js
+++ quasar.conf.js
@@ -86,7 +86,7 @@ module.exports = function(/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ["SessionStorage"]
+      plugins: ["SessionStorage", "Diallog"]
     },
 
     // animations: 'all', // --- includes all animations
